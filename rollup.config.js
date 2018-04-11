423234ce0c5f0a063b4d295675defd5fe93eb76d
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-04-12 00:45:01 +0200

    feat(lib): setup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import commonjs from 'rollup-plugin-commonjs';
+import npm from 'rollup-plugin-node-resolve';
+import * as packageJson from './package.json';
+
+const info = `/*
+ * ${packageJson.library}
+ * Version ${packageJson.version}
+ * ${packageJson.homepage}
+ */
+`;
+
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'GestureEvents',
+    file: 'dist/gesture-events.js',
+    format: 'umd',
+    exports: 'named',
+    banner: info,
+  },
+  plugins: [
+    npm({
+      main: true,
+      jsnext: true,
+      browser: true,
+    }),
+    commonjs(),
+  ],
+};
